dfa027948c1c4523eb67d90733ebdd9bdce9b164
Jungwon Son <60088307+sjungwon@users.noreply.github.com>
2023-08-26 01:06:16 +0900

    Time: 413 ms (77.51%), Space: 17.6 MB (61.51%) - LeetHub

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -1,22 +1,19 @@
 # 그래프?
 # 0번 인덱스를 시작 노드로 생각
 # 가능한 이동 거리의 인덱스들을 연결된 노드로 생각하고 탐색
+# 이동을 1씩만도 할 수 있어서 현재 + max jump한게 마지막 인덱스 넘으면 도달 가능
+# 그럼 그냥 순회하면서 끝까지 닿을 수 있나만 계산하면 됨
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        maxSize = len(nums) - 1;
-        visited = [False] * (maxSize + 1)
-        que = []
-        que.append(0);
-        while(len(que) > 0) :
-            index = que.pop(len(que) - 1)
-            if visited[index] :
-                continue
-            if index == maxSize:
+        maxIndex = 0;
+        for i in range(len(nums) - 1):
+            curJump = i + nums[i]
+            if nums[i] == 0 and maxIndex <= i:
+                return False
+            if curJump >= len(nums) - 1 :
                 return True
-            visited[index] = True;
-            size = min(index+nums[index], maxSize) + 1
-            for i in range(index, size):
-                que.append(i);
-            
-        return False
+            if maxIndex < curJump:
+                maxIndex = curJump
+        
+        return maxIndex >= len(nums) - 1
         
