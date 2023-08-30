f5a9f8351fafe4b4dbb84280f090eb19e3d4ccf6
Jungwon Son <60088307+sjungwon@users.noreply.github.com>
2023-08-30 23:58:34 +0900

    Time: 43 ms (92.71%), Space: 17.1 MB (86.57%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,16 +1,16 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        s = list(filter(lambda c: (c>='A' and c<='Z') or (c>='a' and c<='z') or (c>='0' and c<='9'),s));
-        print(s)
-        
-        s = list(map(lambda c: c.lower(), s))
-        
         i = 0
         j = len(s) - 1
         while i < j:
-            if s[i] != s[j]:
+            if not s[i].isalnum():
+                i+=1
+                continue
+            if not s[j].isalnum():
+                j-=1
+                continue
+            if s[i].lower() != s[j].lower():
                 return False
             i+=1
             j-=1
-            
         return True
