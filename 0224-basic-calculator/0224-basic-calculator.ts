7b541f9311802c59846d8ec22e56c41b6d30de71
Jungwon Son <60088307+sjungwon@users.noreply.github.com>
2023-10-07 17:25:08 +0900

    Time: 250 ms (11.83%), Space: 67.6 MB (5.38%) - LeetHub

---
+++ 0224-basic-calculator/0224-basic-calculator.ts
@@ -0,0 +1,94 @@
+function calculate(s: string): number {
+    const splitStr = s.replaceAll(' ', '').split('');
+    
+    if(!splitStr.includes('-') && !splitStr.includes('+') && !splitStr.includes('(') && !splitStr.includes(')')){
+        const str = splitStr.join('');
+
+        return parseInt(str);
+    }
+    
+    
+    const parsedStr = [];
+    for(let i = 0; i<splitStr.length; i++){
+        if(!isNaN(parseInt(splitStr[i]))){
+            let str = splitStr[i];
+            let j = i+1;
+            while(j < splitStr.length && splitStr[j] <= '9' && splitStr[j]>='0'){
+                str = `${str}${splitStr[j]}`;
+                i=j++;
+            }
+            parsedStr.push(str);
+            continue;
+        }
+        parsedStr.push(splitStr[i]);
+    }
+    
+    // console.log('parsed',parsedStr);
+    
+    const stack = [];
+    
+    for(let i = 0; i<parsedStr.length;i++){
+        const s = parsedStr[i];
+        // console.log(stack, 's',s);
+        if(s==='+'){
+            continue;
+        }
+        // console.log(s);
+        if(s === ')'){
+            const tmp = [];
+            console.log('test',stack);
+             while(stack.length && stack[stack.length - 1] !== '('){
+                 tmp.push(stack.pop());
+             }
+            stack.pop();
+            let result = 0;
+            let pos = 1;
+            while(tmp.length){
+               const elem = tmp.pop();
+                if(elem === '-'){
+                    pos *= -1;
+                    continue;
+                }
+                result+= pos * parseInt(elem);
+                // console.log('result',result);
+                pos = 1;
+            }
+            
+            stack.push(result.toString());
+            
+            continue;
+        }
+      
+        stack.push(s);
+    }
+    
+    // console.log(stack);
+    const parsedStack = [];
+    for(let i = 0; i<stack.length;i++){
+        if(stack[i] !== '-'){
+            parsedStack.push(stack[i]);
+            continue;
+        }
+        
+        if(i+1 >= stack.length){
+            continue;
+        }
+        
+        if(stack[i+1] === '-'){
+            i+=1;
+            continue;
+        }
+        
+        const num = parseInt(stack[++i]) * -1;
+        parsedStack.push(num.toString());
+    }
+    
+    // console.log(parsedStack);
+
+    let result = 0;
+    parsedStack.forEach(s=>{
+        result+=parseInt(s);
+    });
+    // console.log(result);
+    return result;
+};
