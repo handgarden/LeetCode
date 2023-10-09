ee2ed4253739c9345f89ef9f97ad80802d19e7f8
Jungwon Son <60088307+sjungwon@users.noreply.github.com>
2023-10-09 18:27:10 +0900

    Time: 59 ms (33.29%), Space: 42.8 MB (55.48%) - LeetHub

--- 0198-house-robber/0198-house-robber.ts
+++ 0198-house-robber/0198-house-robber.ts
@@ -3,8 +3,11 @@ function rob(nums: number[]): number {
     let result = 0;
     for(let i = 0; i<nums.length;i++){
         let max = 0;
-        for(let j = i-2;j>=0;j--){
-            max = Math.max(max, dp[j]);
+        if(i-2 >= 0){
+            max = dp[i-2];
+        }
+        if(i-3>=0){
+            max = Math.max(dp[i-3], max);
         }
         dp.push(nums[i] + max);
         result = Math.max(dp[i], result);
