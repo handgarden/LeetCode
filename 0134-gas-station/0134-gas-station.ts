5294d16cd201a9ce44a030cd668de39878cb7f45
Jungwon Son <60088307+sjungwon@users.noreply.github.com>
2023-10-11 23:03:55 +0900

    Time: 67 ms (95.37%), Space: 54.3 MB (28.28%) - LeetHub

--- 0134-gas-station/0134-gas-station.ts
+++ 0134-gas-station/0134-gas-station.ts
@@ -1,30 +1,16 @@
 function canCompleteCircuit(gas: number[], cost: number[]): number {
     let sum = 0;
-    const diff = [];
+    let curSum = 0;
+    let start = 0;
     for(let i = 0; i<gas.length;i++){
-        const dif = gas[i] - cost[i];
-        sum += dif;
-        diff.push(dif);
-    }
-    if(sum < 0){
-        return -1;
-    }
-    // console.log(diff);
-    for(let i = 0; i<gas.length;i++){
-        let count = 0;
-        let tmp = 0;
-        for(let j=0;j<gas.length;j++){
-            const index = (i+j) % gas.length;
-            tmp+=diff[index];
-            // console.log(i, index, tmp);
-            if(tmp<0){
-                break;
-            }
-            count++;
-        }
-        if(count == gas.length){
-            return i;
+        const diff = gas[i] - cost[i];
+        sum+=diff;
+        curSum+=diff;
+        if(curSum < 0){
+            start = i+1;
+            curSum=0;
         }
     }
-    return -1;
+    
+    return sum < 0 ? -1 : start % gas.length;
 };
