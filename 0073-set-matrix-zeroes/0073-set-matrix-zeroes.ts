1a29bef82f5c22521dd68bca50c2cb36e9c625e5
handgarden <60088307+handgarden@users.noreply.github.com>
2024-03-31 23:47:04 +0900

    Time: 75 ms (78.70%), Space: 54.7 MB (30.62%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.ts
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.ts
@@ -1,41 +1,52 @@
 /**
  Do not return anything, modify matrix in-place instead.
  */
-
-class Virus {
-    infectX(x: number, matrix: number[][]){
-        
-        for(let i = 0; i<matrix.length; i++){
-            matrix[i][x] = 0;
-        }
+class Index {
+    x: number;
+    y: number;
+    constructor(x:number, y: number){
+        this.x = x;
+        this.y = y;
     }
 
-    infectY(y: number, matrix: number[][]){
-        for(let i = 0; i<matrix[0].length; i++){
-            matrix[y][i] = 0;
+}
+
+class Virus {
+    index: Index;
+    matrix: number[][];
+
+    constructor(index: Index, matrix: number[][]){
+        this.index = index;
+        this.matrix = matrix;
+    }
+
+    infect(){
+        const x = this.index.x;
+        const y = this.index.y;
+        
+        for(let i = 0; i<this.matrix[0].length; i++){
+            this.matrix[y][i] = 0;
+        }
+        
+        for(let i = 0; i<this.matrix.length;i++){
+            this.matrix[i][x] = 0;
         }
     }
 }
 
-
 function setZeroes(matrix: number[][]): void {
-    const VirusXSet = new Set<number>();
-    const VirusYSet = new Set<number>();
+    const virusQue: Virus[] = [];
     
     for(let i = 0; i<matrix.length; i++){
         for(let j = 0; j<matrix[0].length; j++){
             if(!matrix[i][j]){
-                VirusXSet.add(j);
-                VirusYSet.add(i);
+                virusQue.push(new Virus(new Index(j,i), matrix));
             }
         }
     }
-    const virus = new Virus();
-    for(const x of VirusXSet.values()){
-        virus.infectX(x, matrix);
-    }
-    for(const y of VirusYSet.values()){
-        virus.infectY(y, matrix);
-    }
     
+    while(virusQue.length){
+        const virus = virusQue.pop();
+        virus.infect();
+    }
 };
