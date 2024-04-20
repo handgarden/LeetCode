b430ae5270e569a13842ad6aec0f43bf9b1d650f
handgarden <60088307+handgarden@users.noreply.github.com>
2024-04-20 18:52:42 +0900

    Time: 57 ms (87.54%), Space: 53 MB (30.64%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.ts
+++ 0064-minimum-path-sum/0064-minimum-path-sum.ts
@@ -1,32 +1,15 @@
 
-class GridDP {
-    readonly gridDP = [];
-    constructor(maxI: number, maxJ: number){
-        for(let i = 0; i<=maxI; i++){
-            this.gridDP.push(new Array(maxJ + 1).fill(0))
-        }
-    }
-
-    setVal(i: number, j:number, val: number){
-        this.gridDP[i][j] = val;
-    }
-
-    getVal(i: number, j: number){
-        return this.gridDP[i][j];
-    }
-
-    getPrevVal(prevIndexes: number[][]){
-        
-    }
-}
 
 
 function minPathSum(grid: number[][]): number {
     const maxI = grid.length - 1;
     const maxJ = grid[0].length - 1;
-    const dp = new GridDP(maxI, maxJ);
+    const dp = [];
+    for(let i = 0; i<=maxI; i++){
+        dp.push(new Array(maxJ + 1).fill(0))
+    }
 
-    dp.setVal(0,0, grid[0][0]);
+    dp[0][0] = grid[0][0];
     for(let i = 0; i<=maxI; i++){
         for(let j = 0; j<=maxJ; j++){
             if(i == 0 && j==0){
@@ -34,22 +17,22 @@ function minPathSum(grid: number[][]): number {
             }
             const curVal = grid[i][j];
             const prevVal = getPrevVal(dp, i,j);
-            dp.setVal(i,j, prevVal + curVal);
+            dp[i][j] = prevVal + curVal;
         }
     }
     
     
-    return dp.getVal(maxI, maxJ);
+    return dp[maxI][maxJ];
 };
 
-function getPrevVal(dp:GridDP, i: number, j: number){
+function getPrevVal(dp:number[][], i: number, j: number){
     if(j - 1 < 0){
-        return dp.getVal(i-1, j);
+        return dp[i-1][j];
     }
     
     if(i - 1 < 0){
-        return dp.getVal(i, j -1);
+        return dp[i][j -1];
     }
     
-    return Math.min(dp.getVal(i - 1, j), dp.getVal(i,j-1));
+    return Math.min(dp[i - 1][j], dp[i][j-1]);
 }
