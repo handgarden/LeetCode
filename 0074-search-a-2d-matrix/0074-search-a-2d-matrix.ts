0d54ec8109488ba5ffebd4600acf97c34c89e56f
handgarden <60088307+handgarden@users.noreply.github.com>
2024-04-22 23:25:13 +0900

    Time: 94 ms (5.26%), Space: 55.8 MB (5.99%) - LeetHub

---
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.ts
@@ -0,0 +1,17 @@
+function binarySearch(matrix: number[], target: number){
+    console.log(matrix, target)
+    if(matrix.length == 1){
+        return matrix[0] === target;
+    }
+    const pivot = Math.floor(matrix.length / 2);
+    if(matrix[pivot] > target){
+        return binarySearch(matrix.slice(0, pivot), target);
+    }
+    return binarySearch(matrix.slice(pivot),target);
+}
+
+function searchMatrix(matrix: number[][], target: number): boolean {
+    const flatMatrix = matrix.flat();
+    
+    return binarySearch(flatMatrix, target);
+};
